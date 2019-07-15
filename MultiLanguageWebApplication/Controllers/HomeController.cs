6f5346cb6da2659463745cd2f24ed06b70eea9b3
Sullivan008 <kozakpeter0424@gmail.com>
2019-07-15 19:59:22 +0200

    ASP.NET MVC - Multi Language Web Application Example

---
+++ MultiLanguageWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MultiLanguageWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MultiLanguageWebApplication/Controllers/SecondPageController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MultiLanguageWebApplication.Controllers
+{
+    public class SecondPageController : Controller
+    {
+        // GET SecondPage/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
